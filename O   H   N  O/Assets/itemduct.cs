78667cc03d0985cad1b14b00e60f2044d7b4764f
Sebo2205 <61531161+Sebo2205@users.noreply.github.com>
2020-06-22 21:59:30 -0400

    Update itemduct.cs

--- O   H   N  O/Assets/itemduct.cs
+++ O   H   N  O/Assets/itemduct.cs
@@ -24,12 +24,16 @@ public class itemduct : MonoBehaviour
 
     void OnTriggerStay2D(Collider2D col) {
         droppedItem di = col.GetComponent<droppedItem>();
+        entity e = col.GetComponent<entity>();
 
         if(di != null) {
             di.GetComponent<Rigidbody2D>().position += (Vector2)transform.right * transportSpeed * Time.deltaTime;
             di.GetComponent<Rigidbody2D>().position = Vector3.Lerp(di.GetComponent<Rigidbody2D>().position, transform.position, pullToCenterSpeed * Time.deltaTime);
             di.GetComponent<Rigidbody2D>().velocity = Vector2.Lerp(di.GetComponent<Rigidbody2D>().velocity, Vector2.zero, 0.3f);
             s.color = effect;
+        } else if(e != null) {
+            e.GetComponent<Rigidbody2D>().position += (Vector2)transform.right * transportSpeed * Time.deltaTime;
+            //e.GetComponent<Rigidbody2D>().position = Vector3.Lerp(e.GetComponent<Rigidbody2D>().position, transform.position, pullToCenterSpeed * Time.deltaTime);
         }
 
         
